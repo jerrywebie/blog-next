8aa9fc5acb5c90827b1279c3872ac2f660c76718
jerrywebie <jerry.webie@gmail.com>
2022-09-14 23:54:59 +0630

    added most popular

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import CustomLayout from '../layout/CustomLayout'
 import SliderSection from '../components/SliderSection'
+import PostsSection from '../components/PostsSection'
+import MostPopular from '../components/MostPopular'
 
 export default function Home() {
   return (
     <CustomLayout>
       <SliderSection/>
+      <PostsSection/>
+      <MostPopular/>
     </CustomLayout>
   )
 }
