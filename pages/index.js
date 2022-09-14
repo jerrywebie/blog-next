085faa846ee68fc71054b9431446885b32f653fc
jerrywebie <jerry.webie@gmail.com>
2022-09-14 23:14:08 +0630

    rename slider section and remove unused codes

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import Header from '../components/Header'
-// import Footer from '../components/Footer'
-//import styles from '../styles/Home.module.css'
 import CustomLayout from '../layout/CustomLayout'
-import SectionOne from '../components/SectionOne'
+import SliderSection from '../components/SliderSection'
 
 export default function Home() {
   return (
     <CustomLayout>
-      <SectionOne/>
+      <SliderSection/>
     </CustomLayout>
   )
 }
+
